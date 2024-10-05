dc4d6d30d462e0acba0bc6103348413dc52fa923
Makvi-droid <pasbenauro05@gmail.com>
2024-10-05 11:48:36 +0800

    adjusted code

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ $(document).ready(function () {
             <div class="col-md-4 mb-3"> <!-- This ensures cards stack correctly -->
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Book Title: ${title}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Book Author: ${author}</h6>
+                        <h5 class="card-title">Title: ${title}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Author: ${author}</h6>
                         <p class="card-text">Genre: ${genre}</p>
                         <button class="btn btn-danger delete-btn">Delete</button>
                     </div>
